cf074189dca3b6ac7965ec2605a4122a73e66c64
Anujjain2579 <103171741+Anujjain2579@users.noreply.github.com>
2023-04-15 14:09:04 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,14 @@
+import cv2
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+numbers = []
+def read_number_from_image(image_path):
+    image = cv2.imread(image_path)
+    # Do some OpenCV processing to read the number from the image
+    # ...
+    numbers.append(1234)  # Replace this with the actual number you read from the image
+    return numbers
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -9,7 +18,8 @@ def home():
         number = read_number_from_image(image)
         return render_template('result.html', number=number)
     else:
-        return render_template('data.html')
+        return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
