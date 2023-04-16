f74e69e36e079ae595beecc8a84d96cb6e40a0e7
aaryatopiwala <75400069+aaryatopiwala@users.noreply.github.com>
2023-04-15 17:01:24 -0700

    opencv and test

---
+++ Sensors/test.py
@@ -0,0 +1,15 @@
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from PIL import Image
+import requests
+
+# load image from the IAM database (actually this model is meant to be used on printed text)
+url = 'https://cdn.discordapp.com/attachments/1082377351322677320/1096948261434167506/laundry00.png'
+image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+
+processor = TrOCRProcessor.from_pretrained('microsoft/trocr-large-printed')
+model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-large-printed')
+pixel_values = processor(images=image, return_tensors="pt").pixel_values
+
+generated_ids = model.generate(pixel_values)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(generated_text)
